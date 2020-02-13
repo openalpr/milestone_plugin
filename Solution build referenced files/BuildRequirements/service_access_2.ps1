19dbe2a0c92ce1d2345008cc89084ef01ef0d00e
Karen Gabrielyan <gabrielyan@msn.com>
2020-02-13 08:38:32 -0500

    PowerShell script to add logon account for Service (intermediate backup)

---
+++ Solution build referenced files/BuildRequirements/service_access_2.ps1
@@ -0,0 +1,34 @@
+﻿$ServiceAccount = ".\Administrator"
+$ServicePass    = "Password"
+$Computer       = "."
+$ServiceName    = "OpenALPRMilestone"
+
+$ServiceObject  = Get-WmiObject -ComputerName "$Computer" -Query "SELECT * FROM Win32_Service WHERE Name = '$ServiceName'"
+
+$DisplayName = $($ServiceObject | select DisplayName)
+$PathName = $($ServiceObject | select PathName)
+$ServiceType = $($ServiceObject | select ServiceType)
+$ErrorControl = $($ServiceObject | select ErrorControl)
+$StartMode = $($ServiceObject | select StartMode)
+$DesktopInteract = $($ServiceObject | select DesktopInteract)
+$StartName = $($ServiceObject | select StartName)
+$StartPassword = $($ServiceObject | select StartPassword)
+$LoadOrderGroup = $($ServiceObject | select LoadOrderGroup)
+$LoadOrderGroupDependencies = $($ServiceObject | select LoadOrderGroupDependencies)
+$ServiceDependencies = $($ServiceObject | select ServiceDependencies)
+
+Write-Output 	$DisplayName,
+				$PathName,
+				$ServiceType,
+				$ErrorControl,
+				$StartMode,
+				$DesktopInteract,
+				$StartName,
+				$StartPassword,
+				$LoadOrderGroup,
+				$LoadOrderGroupDependencies,
+				$ServiceDependencies
+
+$ServiceObject.stopservice() | out-null
+$ServiceObject.Change($DisplayName,$PathName,$ServiceType,$ErrorControl,$StartMode,$DesktopInteract,$ServiceAccount,$ServicePass,$LoadOrderGroup,$LoadOrderGroupDependencies,$ServiceDependencies)
+$ServiceObject.startservice()
