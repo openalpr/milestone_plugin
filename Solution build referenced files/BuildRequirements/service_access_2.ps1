4f94f478e6e245246b8ca543a77beb7fc828b1b4
Karen Gabrielyan <gabrielyan@msn.com>
2020-02-13 17:50:37 -0500

    PowerShell script to add logon account for Service (intermediate backup)

--- Solution build referenced files/BuildRequirements/service_access_2.ps1
+++ Solution build referenced files/BuildRequirements/service_access_2.ps1
@@ -17,17 +17,35 @@ $LoadOrderGroup = $($ServiceObject | select LoadOrderGroup)
 $LoadOrderGroupDependencies = $($ServiceObject | select LoadOrderGroupDependencies)
 $ServiceDependencies = $($ServiceObject | select ServiceDependencies)
 
-Write-Output 	$DisplayName,
-				$PathName,
-				$ServiceType,
-				$ErrorControl,
-				$StartMode,
-				$DesktopInteract,
-				$StartName,
-				$StartPassword,
-				$LoadOrderGroup,
-				$LoadOrderGroupDependencies,
-				$ServiceDependencies
+##Write-Output 	##$DisplayName,
+				##$PathName,
+				##$ServiceType,
+				##$ErrorControl,
+				##$StartMode,
+				##$DesktopInteract,
+				##$StartName,
+				##$StartPassword,
+				##$LoadOrderGroup,
+				##$LoadOrderGroupDependencies,
+				##$ServiceDependencies
+
+switch($ServiceType.ServiceType.ToString()){
+    "Kernel Driver" {$ServiceType = 0x1}
+    "File System Driver" {$ServiceType = 0x2}
+    "Adapter" {$ServiceType = 0x4}
+    "Recognizer Driver" {$ServiceType = 0x8}
+    "Own Process" {$ServiceType = 0x10}
+    "Share Process" {$ServiceType = 0x20}
+    "Interactive Process" {$ServiceType = 0x100}
+}
+
+switch($ErrorControl.ErrorControl.ToString()){
+    "Ignore"   {$ErrorControl = 0x0}
+    "Normal"   {$ErrorControl = 0x1}
+    "Severe"    {$ErrorControl = 0x2}
+    "Critical"  {$ErrorControl = 0x3}
+}
+
 
 $ServiceObject.stopservice() | out-null
 $ServiceObject.Change($DisplayName,$PathName,$ServiceType,$ErrorControl,$StartMode,$DesktopInteract,$ServiceAccount,$ServicePass,$LoadOrderGroup,$LoadOrderGroupDependencies,$ServiceDependencies)
